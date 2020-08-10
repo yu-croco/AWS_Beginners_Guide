5d0bccd15c46e4283d9604a54804e6c38dcdc41a
yu-croco <yuki.kita22@gmail.com>
2020-08-10 15:39:30 +0900

    add web server

---
+++ 3_container/src/bin/build_image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker build -t sample-node-app .
---
+++ 3_container/src/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World from node.js');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
