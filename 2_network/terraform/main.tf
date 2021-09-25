96a7328bc82906e9542e9cf8ed7b50eff6f61038
yu-croco <yuki.kita22@gmail.com>
2021-09-25 20:10:27 +0900

    update versions

--- 2_network/terraform/main.tf
+++ 2_network/terraform/main.tf
@@ -1,9 +1,24 @@
 provider "aws" {
   region  = "ap-northeast-1"
-  version = "~> 3.0"
+
+  default_tags {
+    tags = {
+      Env                = "dev"
+      IsTerraformManaged = "true"
+      System             = "infra-study"
+    }
+  }
 }
+
 terraform {
-  required_version = "= 0.13.4"
+  required_version = "= 1.0.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.60"
+    }
+  }
 }
 
 resource "aws_vpc" "infra_study_vpc" {
